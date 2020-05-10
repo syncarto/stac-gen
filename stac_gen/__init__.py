e714e8394bd22d9358eb5eef785356137aba7515
Nick Guries <guries@earthlinginteractive.com>
2020-05-10 13:35:13 -0500

    init py tweaks

--- stac_gen/__init__.py
+++ stac_gen/__init__.py
@@ -1,3 +1,14 @@
-from .create_stac_catalog import create_stac_catalog
+
+import pkgutil
+import pprint
+import sys
+
+__path__ = pkgutil.extend_path(__path__, __name__)
+
+sys.path += __path__
+#print ('stacgen.__path__ after:')
+#pprint.pprint(sys.path)
+
 from . import satstac
+from .create_stac_catalog import create_stac_catalog
 __all__ = ['create_stac_catalog','satstac']
